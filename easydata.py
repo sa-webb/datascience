d922df423e67de0786ab433fcb7712b0347ff6ce
Austin Webb <sawebb30@gmail.com>
2018-03-01 18:52:46 -0500

    Update easydata.py

--- easydata.py
+++ easydata.py
@@ -2,12 +2,31 @@ import pandas as pd
 import numpy as np
 
 data = pd.read_csv('test1.csv')
+
 print(data.columns)
 
-test_array = data.columns
+features = data.columns
 
-print(test_array[0])
-column1 = test_array['Column1']
 
-print(column1.values)
-print(column1.mean())
+user = []
+n_columns = int(input('Enter number of columns: '))
+for i in range(0,n_columns):
+	column_title = input('Enter exact column title: ')
+	user.append(column_title)
+
+column1_input = user[0]
+column2_input = user[1]
+
+
+# Using pandas to get columns
+column1 = data[column1_input]
+column2 = data[column2_input]
+
+print("column1 ",column1.values)
+print("column2 ",column2.values)
+
+column1_mean = column1.mean()
+column2_mean = column2.mean()
+
+print("mean: ", column1_mean)
+print("mean: ", column2_mean)
