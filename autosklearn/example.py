e410aa8b24780e51c14e19c398b91631007f310b
Austin Webb <sawebb30@gmail.com>
2018-03-05 22:35:22 -0500

    Create example.py

---
+++ autosklearn/example.py
@@ -0,0 +1,17 @@
+# provided example for autosklearn
+# This will run for one hour and should result in an accuracy above 0.98
+# auto-sklearn is an automated machine learning toolkit and a drop-in replacement for a scikit-learn estimator
+
+import autosklearn.classification
+import sklearn.model_selection
+import sklearn.datasets
+import sklearn.metrics
+
+X, y = sklearn.datasets.load_digits(return_X_y=True)
+X_train, X_test, y_train, y_test = \
+        sklearn.model_selection.train_test_split(X, y, random_state=1)
+
+automl = autosklearn.classification.AutoSklearnClassifier()
+automl.fit(X_train, y_train)
+y_hat = automl.predict(X_test)
+print("Accuracy score", sklearn.metrics.accuracy_score(y_test, y_hat))
